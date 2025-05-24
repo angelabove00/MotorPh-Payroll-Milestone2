287a5db841ad9466383a4c314f9cd7c0d1388d6d
appanlilio <lr.apanlilio@mmdc.mcl.edu.ph>
2025-05-25 05:08:47 +0900

    update from test repository

--- src/model/Deductions.java
+++ src/model/Deductions.java
@@ -1,3 +1,5 @@
+package model;
+
 public class Deductions {
     private double sss;
     private double philHealth;
@@ -5,23 +7,47 @@ public class Deductions {
     private double withholdingTax;
     private double otherDeductions;
 
-    // Getters & setters for each field
-    public double getSss() { return sss; }
-    public void setSss(double sss) { this.sss = sss; }
+    public double getSss() { 
+        return sss; 
+    }
 
-    public double getPhilHealth() { return philHealth; }
-    public void setPhilHealth(double philHealth) { this.philHealth = philHealth; }
+    public void setSss(double sss) { 
+        this.sss = sss; 
+    }
 
-    public double getPagIbig() { return pagIbig; }
-    public void setPagIbig(double pagIbig) { this.pagIbig = pagIbig; }
+    public double getPhilHealth() { 
+        return philHealth; 
+    }
 
-    public double getWithholdingTax() { return withholdingTax; }
-    public void setWithholdingTax(double withholdingTax) { this.withholdingTax = withholdingTax; }
+    public void setPhilHealth(double philHealth) { 
+        this.philHealth = philHealth; 
+    }
 
-    public double getOtherDeductions() { return otherDeductions; }
-    public void setOtherDeductions(double otherDeductions) { this.otherDeductions = otherDeductions; }
+    public double getPagIbig() { 
+        return pagIbig; 
+    }
+
+    public void setPagIbig(double pagIbig) { 
+        this.pagIbig = pagIbig; 
+    }
+
+    public double getWithholdingTax() { 
+        return withholdingTax; 
+    }
+
+    public void setWithholdingTax(double withholdingTax) { 
+        this.withholdingTax = withholdingTax; 
+    }
+
+    public double getOtherDeductions() { 
+        return otherDeductions; 
+    }
+
+    public void setOtherDeductions(double otherDeductions) { 
+        this.otherDeductions = otherDeductions; 
+    }
 
     public double getTotalDeductions() {
         return sss + philHealth + pagIbig + withholdingTax + otherDeductions;
     }
-}
+} 
