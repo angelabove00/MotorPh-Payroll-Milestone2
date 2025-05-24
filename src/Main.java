0a3acfc3a9e4dba0013a26f2ad2ece95c494d41d
appanlilio <lr.apanlilio@mmdc.mcl.edu.ph>
2025-05-25 05:04:28 +0900

    cleanup and update main java class
    based on test repo changes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-import java.time.LocalDate;
-import java.time.LocalTime;
+//
+// MotorPH Employee Application Portal Main Class
+//
+
+// all UI-related classes are now moved to view package
+// all modules moved to model package
+import view.MainInterface;
 
 public class Main {
     public static void main(String[] args) {
-        MainInterface my = new MainInterface();
-        my.setVisible(true);
+        MainInterface window = new MainInterface();
+        window.setVisible(true);
     }
 }
