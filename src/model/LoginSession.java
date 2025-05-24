40d233355a663b64ef61a8fc25a66dbba95f25cd
appanlilio <lr.apanlilio@mmdc.mcl.edu.ph>
2025-05-25 05:06:53 +0900

    update from test repository

--- src/model/LoginSession.java
+++ src/model/LoginSession.java
@@ -1,13 +1,31 @@
+package model;
+
 import java.time.LocalTime;
 
 public class LoginSession {
     private LocalTime loginTime;
+    private LocalTime logoutTime;
+    private boolean isActive;
 
-    public LocalTime getLoginTime() { return loginTime; }
-    public void setLoginTime(LocalTime loginTime) { this.loginTime = loginTime; }
-
-    public boolean isLateLogin() {
-        // TODO: implement grace‑period logic
-        return false;
+    public LocalTime getLoginTime() {
+        return loginTime;
     }
-}
+
+    public void setLoginTime(LocalTime time) {
+        this.loginTime = time;
+        this.isActive = true;
+    }
+
+    public LocalTime getLogoutTime() {
+        return logoutTime;
+    }
+
+    public void setLogoutTime(LocalTime logoutTime) {
+        this.logoutTime = logoutTime;
+        this.isActive = false;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+} 
