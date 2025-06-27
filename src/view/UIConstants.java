5d6a6f1ade74e4ce3e683d33e14b5e3dd19d9f05
appanlilio <lr.apanlilio@mmdc.mcl.edu.ph>
2025-06-27 12:18:21 +0800

    Update UIConstants.java -- adjust size and button font

--- src/view/UIConstants.java
+++ src/view/UIConstants.java
@@ -13,14 +13,15 @@ public class UIConstants {
     // Fonts
     public static final Font HEADER_FONT = new Font("SansSerif", Font.BOLD, 18);
     public static final Font LABEL_FONT = new Font("SansSerif", Font.PLAIN, 12);
+    public static final Font BUTTON_FONT = new Font("SansSerif", Font.PLAIN, 12);
 
     // Dimensions
     public static final Dimension MAIN_WINDOW_SIZE = new Dimension(800, 600);
-    public static final Dimension DETAIL_WINDOW_SIZE = new Dimension(500, 700);
+    public static final Dimension DETAIL_WINDOW_SIZE = new Dimension(700, 700);
     public static final Dimension NEW_EMPLOYEE_WINDOW_SIZE = new Dimension(500, 600);
     public static final int STANDARD_FIELD_WIDTH = 250;
 
     // Borders
     public static final Border EMPTY_BORDER = BorderFactory.createEmptyBorder(20, 20, 20, 20);
     public static final Border LINE_BORDER = BorderFactory.createLineBorder(Color.LIGHT_GRAY);
-} 
+}
